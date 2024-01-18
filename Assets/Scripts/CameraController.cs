c9177a1af6ec46c4fe0e26d0840921f61044cf9d
Machima1107 <machima.nant@bumail.net>
2024-01-18 10:33:51 +0700

    Camera-zoomFN-rotateFN

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -16,17 +16,38 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
 
+    [Header("Zoom")]
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] private float zoomModifier;
+
+    [SerializeField] private float minZoomDist;
+    [SerializeField] private float maxZoomDist;
+
+    [SerializeField] private float dist;
+
+    [Header("Rotate")]
+    [SerializeField] private float rotationAmount;
+    [SerializeField] private Quaternion newRotation;
+
+
+
     public static CameraController instance;
 
     void Awake()
     {
         instance = this;
         cam = Camera.main;
+        newRotation = transform.rotation;
+        rotationAmount = 1;
     }
 
     void Start()
     {
         moveSpeed = 50;
+
+        zoomSpeed = 25;
+        maxZoomDist = 50;
+        minZoomDist = 15;
     }
 
     private void MoveByKB()
@@ -48,9 +69,40 @@ public class CameraController : MonoBehaviour
         return pos;
     }
 
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = 0.01f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = -0.01f;
+
+        dist = Vector3.Distance(transform.position, cam.transform.position);
+        
+        if (dist < minZoomDist && zoomModifier > 0f)
+            return; //too close
+        else if (dist > maxZoomDist && zoomModifier < 0f)
+            return; //too far
+        
+        cam.transform.position += cam.transform.forward * zoomModifier * zoomSpeed;
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+
+        if (Input.GetKey(KeyCode.E))
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * moveSpeed);
+    }
+
     // Update is called once per frame
     void Update()
     {
         MoveByKB();
+        Zoom();
+        Rotate();
     }
 }
