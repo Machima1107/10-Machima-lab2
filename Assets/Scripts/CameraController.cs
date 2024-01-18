79707c8bb73aab809f5d926105a9f362d8d42925
Machima1107 <machima.nant@bumail.net>
2024-01-18 10:08:00 +0700

    Camera-move-wasd

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,52 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Camera cam;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Transform conner1;
+    [SerializeField] private Transform conner2;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
+    void Awake()
+    {
+        instance = this;
+        cam = Camera.main;
+    }
+
     void Start()
     {
-        
+        moveSpeed = 50;
+    }
+
+    private void MoveByKB()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+
+        Vector3 dir = (transform.forward * zInput) + (transform.right * xInput);
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+        transform.position = Clamp(conner1.position, conner2.position);
+    }
+
+    private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
+    {
+        Vector3 pos = new Vector3(Mathf.Clamp(transform.position.x, lowerLeft.x, topRight.x),
+        transform.position.y, Mathf.Clamp(transform.position.z, lowerLeft.z, topRight.z));
+
+        return pos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveByKB();
     }
 }
