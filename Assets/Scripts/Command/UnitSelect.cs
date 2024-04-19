b0a859d0bb28d762de1844c5810c51bce78202ff
Machima <machima.nant@bumail.net>
2024-04-19 20:33:14 +0700

    27.3

--- Assets/Scripts/Command/UnitSelect.cs
+++ Assets/Scripts/Command/UnitSelect.cs
@@ -81,6 +81,10 @@ public class UnitSelect : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             ReleaseSelectionBox(Input.mousePosition);
+
+            if (IsPointerOverUIObject())
+                return;
+
             TrySelect(Input.mousePosition);
         }
 
@@ -285,4 +289,13 @@ public class UnitSelect : MonoBehaviour
         }
     }
 
+    //When Touching UI
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
 }
